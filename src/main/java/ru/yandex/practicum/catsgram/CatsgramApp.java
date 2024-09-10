7359d4c11ea79da68e5b99d4f817cd1ad8c029c0
Daniil Andreev <daniiland9@yandex.ru>
2024-09-10 23:26:59 +0300

    Создать Catsgram.

---
+++ src/main/java/ru/yandex/practicum/catsgram/CatsgramApp.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.catsgram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import ru.yandex.practicum.catsgram.model.Post;
+
+
+@SpringBootApplication
+public class CatsgramApp {
+    public static void main(final String[] args) {
+        SpringApplication.run(CatsgramApp.class, args);
+
+
+    }
+}
